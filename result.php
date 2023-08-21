68d3858b6525025789cdf0e744e8fe362774da91
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-08-21 16:08:44 +0200

    Some test array + associative arrays

--- result.php
+++ result.php
@@ -22,6 +22,24 @@
         echo $testo . strlen($testo);
         echo '<br>';
         echo str_replace('ass', '***', $password);
+        echo '<br>';
+        echo [1,2,3];
+        var_dump([1,2,3]);
+        echo '<br>';
+        echo [
+            'macchina' => 'blu'
+        ];
+        echo '<br>';
+        var_dump([
+            'macchina' => 'blu'
+        ]);
+        var_dump([
+            'macchina' => [1,2,3],
+            'macchine' => [
+                'fiat' => true,
+                'mercedes' => false
+            ]
+        ]);  
         ?>
     </h2>
 </body>
